1b22a325842d3b102c42faf2d8741a06c31e18d5
Isaabomohana <mrissaam@outlook.com>
2018-04-10 15:30:44 +0200

    add 2 sections

--- js/init.js
+++ js/init.js
@@ -2,5 +2,16 @@
   $(function(){
     $('.sidenav').sidenav();
     $('.slider').slider();
+    $('.scrollspy').scrollSpy();
+    $('input.autocomplete').autocomplete({
+      data: {
+        "Paris": null,
+        "London": null,
+        "Amsterdam": null,
+        "Berlin": null,
+        "Hamburg": null,
+        "München": null
+      },
+    });
   }); // end of document ready
 })(jQuery); // end of jQuery name space
